7565a64751b7f9d646d3aa1c7491bcf6d38f7563
LINA PATRICIA DAVID GONZALEZ <linadavid212820@correo.itm.edu.co>
2020-11-01 13:19:58 -0500

    Full entities pink world

---
+++ PinkWorld.Web/Data/Entities/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PinkWorld.Web.Data.Entities
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        //TODO change path local
+        [Display(Name = "Image")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44357/images/no-image.png"
+            : $"https://pinkworld.blob.core.windows.net/images/{ImageId}";
+
+    }
+}
---
+++ PinkWorld.Web/Data/Entities/UserAnswer.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace PinkWorld.Web.Data.Entities
+{
+    public class UserAnswer
+    {
+
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public Answer Answer { get; set; }
+
+        [JsonIgnore]
+        public User User { get; set; }
+    }
+}
